b736dff2a650caa16174c49328dab4b07da96cb8
Danny <yanez.daniel0101@gmail.com>
2018-03-22 22:02:16 -0500

    inital commit

---
+++ apps/store/products.py
@@ -0,0 +1,22 @@
+items = [
+    {
+        "id": 1,
+        "name": "Bank T-Shirt",
+        "price": 19.99,
+    },
+    {
+        "id": 2,
+        "name": "Bank Sweater",
+        "price": 29.99
+    },
+    {
+        "id": 3,
+        "name": "Bank Cup",
+        "price": 4.99
+    },
+    {
+        "id": 4,
+        "name": "Bank Premium Pen",
+        "price": 49.99
+    }
+]
